2ed933b21d957d45edef58f903af5d7ea9594194
smandekar1 <sam@Sam-PC.kdkforging.local>
2015-10-07 16:04:06 -0500

    added php

---
+++ contact.php
@@ -0,0 +1,57 @@
+<?php
+if($_POST["message"]) {
+    mail("slmahisekar@gmail.com", "Form to email message", $_POST["message"], "From: contactform@github.io");
+}
+?>
+
+
+<form method="post" action="contact.php">
+    <textarea name="message"></textarea>
+    <input type="submit">
+</form>
+
+
+<?php
+
+if($_POST["submit"]) {
+    $recipient="slmahisekar@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?><!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Contact form to email</title>
+</head>
+
+<body>
+
+    <?=$thankYou ?>
+
+    <form method="post" action="contact.php">
+        <label>Name:</label>
+        <input name="sender">
+
+        <label>Email address:</label>
+        <input name="senderEmail">
+
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+
+</body>
+
+</html>
