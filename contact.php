4de0aa9dd65a7a044265e27e10762ee6d9810c85
smandekar1 <sam@Sam-PC.kdkforging.local>
2015-10-07 16:20:30 -0500

    contact update

--- contact.php
+++ contact.php
@@ -1,45 +1,22 @@
 
 
 <?php
-
-if($_POST["submit"]) {
-    $recipient="slmahisekar@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
+if($_POST["message"]) {
+    mail("slmahisekar@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
+?>
 
-?><!DOCTYPE html>
+
+<!doctype=HTML>
 
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Contact form to email</title>
-</head>
 
 <body>
 
-    <?=$thankYou ?>
-
-    <form method="post" action="contact.php">
-        <label>Name:</label>
-        <input name="sender">
-
-        <label>Email address:</label>
-        <input name="senderEmail">
-
-        <label>Message:</label>
-        <textarea rows="5" cols="20" name="message"></textarea>
-
-        <input type="submit" name="submit">
-    </form>
+<form method="post" action="contact.php">
+    <textarea name="message"></textarea>
+    <input type="submit">
+</form>
 
 </body>
 
